35367eafe1d80b8bd1c8efae14681755209dd0ce
huang-husky <h_u_a_n_g_017@qq.com>
2026-04-06 10:26:04 +0800

    fix: update p33.cpp to match page style (c1/c2, remove comments)

--- blog/coding/source/p33.cpp
+++ blog/coding/source/p33.cpp
@@ -2,34 +2,31 @@
 using namespace std;
 
 int main(){
-    int n, m, q, k;
+    int n,m,q,k;
     cin>>n>>m>>q>>k;
 
-    bool rowLine[1001] = {false};
-    bool colLine[1001] = {false};
+    bool rowLine[1001]={false};
+    bool colLine[1001]={false};
 
-    // 读入 q 个行号
     for(int c=0;c<q;c++){
         int row;
         cin>>row;
         rowLine[row]=true;
     }
 
-    // 读入 k 个列号
-    for(int d=0;d<k;d++){
+    for(int c=0;c<k;c++){
         int col;
         cin>>col;
         colLine[col]=true;
     }
 
-    // 双重循环输出棋盘
-    for(int e=1;e<=n;e++){
-        for(int f=1;f<=m;f++){
-            if(rowLine[e]&&colLine[f]){
+    for(int c1=1;c1<=n;c1++){
+        for(int c2=1;c2<=m;c2++){
+            if(rowLine[c1]&&colLine[c2]){
                 cout<<"+";
-            }else if(rowLine[e]&&!colLine[f]){
+            }else if(rowLine[c1]){
                 cout<<"-";
-            }else if(!rowLine[e]&&colLine[f]){
+            }else if(colLine[c2]){
                 cout<<"|";
             }else{
                 cout<<"0";
