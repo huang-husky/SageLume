f6de0731985a2131ba0240c815c39f615d8020df
huang-husky <h_u_a_n_g_017@qq.com>
2026-04-20 09:07:27 +0800

    chore: update p52.cpp source

--- blog/coding/source/p52.cpp
+++ blog/coding/source/p52.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+// 快速幂
+int qpow(long long b, long long n, long long mod) {
+    b%=mod;  
+    long long ans=1%mod;
+    while (n) {             
+        if (n & 1) {    
+            ans = ans * b % mod;
+        }
+        n >>= 1;   
+        b = b * b % mod; 
+    } 
+    return ans;             
+}
 
+int main() {
+    long long b,n,k;
+    cin>>b>>n>>k;
+    cout<<qpow(b,n,k);
+    
     return 0;
 }
